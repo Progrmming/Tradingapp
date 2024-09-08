63c2a674e83ee7760b96de9185174b6a3edae29e
dhamu <dhamukotti123@gmail.com>
2024-09-08 15:18:04 +0530

    add  login page

--- src/app/(Home)/page.jsx
+++ src/app/(Home)/page.jsx
@@ -38,7 +38,7 @@ function Page() {
             <h1>Welcome to V Power International.</h1>
             <p style={{textAlign:'justify'}}>It's a simple business method that can earn you a fantastic high income. This business model is created entirely by Indians and is headquartered in Delhi. Currently operating as the fastest growing business in many important cities of India. This human chain business is well received by the people as it is able to generate very high revenue in a short period of time. So we wish you to join this business immediately and earn very high income.</p>
             <div className="hero-buttons">
-              <Link  href={{pathname:'/Login'}} className="btn" >Get Started</Link>
+              <Link  href={{pathname:'/login'}} className="btn" >Get Started</Link>
             </div>
         </div>
         <div className="hero-img">
